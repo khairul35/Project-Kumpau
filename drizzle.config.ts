8e47fa7d152967a2c7adb22e79056f9cb3a4291b
Khairul Habib Bin Samaon <57700102+khairul35@users.noreply.github.com>
2024-02-04 12:25:28 +0800

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from "drizzle-kit";
+import type { Config } from 'drizzle-kit';
+require('dotenv').config();
 
-export default defineConfig({
+
+const connectionString = process.env.DATABASE_URL as string;
+
+if (!connectionString) {
+    console.log('no connection string');
+    process.exit();
+}
+
+export default {
     schema: "./app/drizzle/schema.ts",
     driver: "pg",
     dbCredentials: {
-        connectionString: process.env.DATABASE_URL || '',
+        connectionString,
     },
     verbose: true,
     strict: true,
-})
+} satisfies Config;
